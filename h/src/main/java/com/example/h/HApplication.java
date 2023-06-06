7c1be6fc1bbaf4b35906fedbc49f7096812ea6eb
竹影清风 <76869210+zhuyingqingfeng3@users.noreply.github.com>
2023-06-07 00:25:00 +0800

    Add files via upload

---
+++ h/src/main/java/com/example/h/HApplication.java
@@ -0,0 +1,13 @@
+package com.example.h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HApplication.class, args);
+    }
+
+}
